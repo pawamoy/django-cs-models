667a93c8b8a75453b770782a701e8ea597131663
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-12-09 19:54:09 +0100

    Update template branch

--- tests/test_csmodels.py
+++ tests/test_csmodels.py
@@ -10,12 +10,16 @@ import csmodels
 
 
 class MainTestCase(TestCase):
-    """Main Django test case"""
+    """Main Django test case."""
+
     def setUp(self):
+        """Setup method."""
         pass
 
     def test_main(self):
+        """Main test method."""
         assert csmodels
 
     def tearDown(self):
+        """Tear down method."""
         pass
